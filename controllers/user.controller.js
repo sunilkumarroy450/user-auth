364a789abf7b00e36c4397ef2a3f0c5b3f46a556
Sunil Kumar <sunilroy450@gmail.com>
2024-03-20 00:59:20 +0530

    feat: token generation added while login and sign_up

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,6 @@
 const userModel = require("../models/user.model");
 const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 exports.create_user = async (req, res) => {
   try {
@@ -7,7 +8,13 @@ exports.create_user = async (req, res) => {
     const hashedPassword = await bcryptjs.hash(password, 10);
     const user = new userModel({ name, email, password: hashedPassword });
     await user.save();
-    return res.status(200).send({ user, msg: "User created Successfully" });
+    // Generate token for the newly created user
+    const token = jwt.sign({ id: user._id, name: user.name }, "SECRET", {
+      expiresIn: "7 days",
+    });
+    return res
+      .status(200)
+      .send({ user, msg: "User created Successfully", token });
   } catch (error) {
     console.log(error);
     return res.status(500).send({ error: "Internal Server Error" });
@@ -26,11 +33,32 @@ exports.login_user = async (req, res) => {
     );
     if (!isPasswordValid)
       return res.status(401).send({ msg: "Invalid Password" });
+    const token = jwt.sign(
+      { id: existingUser._id, name: existingUser.name },
+      "SECRET",
+      { expiresIn: "7 days" }
+    );
+    const refreshToken = jwt.sign(
+      { id: existingUser._id, name: existingUser.name },
+      "REFRESHSECRET",
+      {
+        expiresIn: "28 days",
+      }
+    );
     return res
       .status(200)
-      .send({ msg: "Login successful", user: existingUser });
+      .send({ msg: "Login successful", token, refreshToken });
   } catch (error) {
     console.log(error);
     return res.status(500).send({ error: "Internal Server Error" });
+  }
+};
+
+exports.refresh_token = async (req, res) => {
+  try {
+    const { refresh_token } = req.headers;
+  } catch (error) {
+    console.error(error.message);
+    return res.status(401).send({ error: "Invalid or expired refresh token" });
   }
 };
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { create_user, login_user } = require("../controllers/user.controller");
+const {
+  create_user,
+  login_user,
+  refresh_token,
+} = require("../controllers/user.controller");
 
 const router = express.Router();
 
 router.post("/", create_user);
 router.post("/login", login_user);
+router.post("/refresh", refresh_token);
 
 module.exports = router;
